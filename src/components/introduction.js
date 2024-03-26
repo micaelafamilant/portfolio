fc5b19924d904482d635a6d1535504a18b45c7a8
Micaela <mmlfamilant@gmail.com>
2024-03-25 21:40:17 -0400

    introduction section set up

---
+++ src/components/introduction.js
@@ -0,0 +1,10 @@
+const introduction = () => {
+    return (
+        <div>
+        <h1>Welcome to our page!</h1>
+        <p>Here you can find the best products at the best prices.</p>
+        </div>
+    );
+};
+
+export default introduction;
